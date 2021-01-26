642fc1b580553ffde54b3c0d694f5dd27f9cb3f1
赵建伟 <zhaojianwei@ezbuy.com>
2021-01-26 18:24:24 +0800

    first commit

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.GET("/hello", func(c echo.Context) error {
+	e.GET("/api/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":11000"))
